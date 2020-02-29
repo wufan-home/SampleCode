c224a963d99c7d508609e897b6c1ab3259032cbd
Fan Wu <deepexploring@gmail.com>
2020-02-29 15:41:23 -0800

    Create client.h

---
+++ network/tcp/c_cpp/client.h
@@ -0,0 +1,23 @@
+#ifndef __CLIENT_H__
+#define __CLIENT_H__
+
+#include <cstring>
+#include <iostream>
+
+class TCPConnection {
+public:
+    TCPConnection(char ip[], unsigned short port, bool isIpV4);
+
+    int connect();
+    void disconnect();
+    int send(char byteStream);
+private:
+    char _ipV4[16];  // C-format string: the content is 12 digits and 3 dots.
+    char _ipV6[16];  // The size is not correct!!!
+    unsigned short _port;  // Range: 0 to 65535. Reserved: 0 to 1023.
+
+    bool _isIpV4;
+    bool _isConnected;
+};
+
+#endif
