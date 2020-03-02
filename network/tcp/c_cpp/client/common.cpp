f5eb47e6fe6902b55293b979ad89af30873a6a15
Fan Wu <deepexploring@gmail.com>
2020-03-01 16:24:33 -0800

    Create common.cpp

---
+++ network/tcp/c_cpp/client/common.cpp
@@ -0,0 +1,16 @@
+/*
+ *
+ * */
+
+#include "common.h" 
+#include <cstdlib>
+
+unsigned short Common::getPortNumber(char portString[]) 
+{ 
+    return std::atoi(portString);
+} 
+
+TCPConnectionInfo Common::parstDataFile(char fileName[]) 
+{ 
+
+} 
