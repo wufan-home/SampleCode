f7e206eedb2735d12b647a4e0bd25b9ac0599343
Fan Wu <deepexploring@gmail.com>
2020-03-01 16:25:09 -0800

    Update start.cpp

--- network/tcp/c_cpp/client/start.cpp
+++ network/tcp/c_cpp/client/start.cpp
@@ -1,31 +1,35 @@
-follow_1-129.213.128.84:~/network/tcp/cpp > clear
-/*
- *
- *
- * */
+/* 
+ * 
+ * 
+ * */ 
 
-#include <iostream>
-#include <stdlib.h>
-#include <string>
+#include <iostream> 
+#include <stdlib.h>  
+#include <string> 
 
-#include "client.h"
+#include "client.h" 
+#include "common.h" 
 
-int main(int argn, char *argv[])
+int main(int argn, char *argv[]) 
 {
-    char ip[30];
-    unsigned short port = 0;
-    bool isIpV4 = true;
+    TCPConnectionInfo connectiongInfo;
 
-    switch(argn)
-    {
-        case 3:
-            std::cout << "Parsing data from the file " << argv[2] << std::endl;
+    switch(argn) 
+    { 
+        case 3: 
+            std::cout << "Parsing data from the file " << argv[2] << std::endl; 
             break;
+        case 7:
         case 9:
-            std::cout << "The host ip = " << argv[2]
-                      << ", port = " << argv[4]
-                      << ", version = " << argv[6] << "."
+            std::cout << "The host ip = " << argv[2] 
+                      << ", port = " << argv[4] 
+                      << ", version = " << argv[6] << "." 
                       << std::endl;
+            
+            strcpy(connectiongInfo.ip, argv[2]); 
+            connectiongInfo.port = Common::getPortNumber(argv[4]); 
+            connectiongInfo.isIpV4 = (strcmp(argv[6], "4") == 0);
+            
             break;
         default:
             std::cout << "ERROR: Incorrect parameter format." << std::endl;
@@ -34,10 +38,14 @@ int main(int argn, char *argv[])
             return 0;
     }
 
-    TCPConnection *connection = new TCPConnection(ip, port, isIpV4);
-    connection->connect();
-    connection->connect();
+    std::cout << connectiongInfo.ip << ", " << connectiongInfo.port << ", " << connectiongInfo.isIpV4 << std::endl;
+
+    TCPConnection *connection = new TCPConnection(connectiongInfo.ip, connectiongInfo.port, connectiongInfo.isIpV4);
+    connection->init();
+    
+    
     connection->disconnect();
+    delete connection;
 
     return 1;
-}
+} 
