f47649a8473ab09e2a7062708002060fd7df7e15
Fan Wu <deepexploring@gmail.com>
2020-03-01 16:23:36 -0800

    Update client.h

--- network/tcp/c_cpp/client/client.h
+++ network/tcp/c_cpp/client/client.h
@@ -1,23 +1,32 @@
-#ifndef __CLIENT_H__
+#ifndef __CLIENT_H__ 
 #define __CLIENT_H__
 
 #include <cstring>
 #include <iostream>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-class TCPConnection {
+#include "common.h"
+
+class TCPConnection { 
 public:
-    TCPConnection(char ip[], unsigned short port, bool isIpV4);
+    TCPConnection(char ip[], unsigned short port, bool isIpV4); 
 
-    int connect();
+    int init();
     void disconnect();
-    int send(char byteStream);
+    
+    int read();
+    int write(char byteStream);
 private:
     char _ipV4[16];  // C-format string: the content is 12 digits and 3 dots.
     char _ipV6[16];  // The size is not correct!!!
     unsigned short _port;  // Range: 0 to 65535. Reserved: 0 to 1023.
+    
+    int _sockFd;
 
     bool _isIpV4;
-    bool _isConnected;
+    bool _isConnected; 
 };
 
-#endif
+#endif 
