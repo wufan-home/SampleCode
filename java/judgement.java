ddf6f64ad1298ce3b3e2fc2e53a0d2895064d4b3
Fan Wu <deepexploring@gmail.com>
2021-02-01 12:32:22 -0800

    Create judgement.java

---
+++ java/judgement.java
@@ -0,0 +1,11 @@
+/*
+    This function will return a true/false value based on the 
+    string stored in A.
+    It has two safty check:
+    1 - If A is null, the default value is false.
+    2 - If A does not contain the key, the default return value is false.
+    
+    The most significant part is to use the function Boolean.valueOf to convert the string to be Boolean.
+*/
+
+A == null ? false : Boolean.valueOf(A.getFuncOrDefault(/*getKeyFunc*/, /*DefaultValueFunction*/)).getValue());
